e495b2737b16124a936002a0e204bda0a1608f12
Jordan Harband <ljharb@gmail.com>
2021-10-03 00:19:42 -0700

    [Robustness] use call-bound `Array.prototype.push`

--- implementation.js
+++ implementation.js
@@ -3,13 +3,14 @@
 var RequireObjectCoercible = require('es-abstract/2021/RequireObjectCoercible');
 var callBound = require('call-bind/callBound');
 var $isEnumerable = callBound('Object.prototype.propertyIsEnumerable');
+var $push = callBound('Array.prototype.push');
 
 module.exports = function entries(O) {
 	var obj = RequireObjectCoercible(O);
 	var entrys = [];
 	for (var key in obj) {
 		if ($isEnumerable(obj, key)) { // checks own-ness as well
-			entrys.push([key, obj[key]]);
+			$push(entrys, [key, obj[key]]);
 		}
 	}
 	return entrys;
